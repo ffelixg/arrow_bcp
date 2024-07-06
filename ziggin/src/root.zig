bdcd596c0f2e141023500c4bcf1dbcf223db3e6a
ffelixg <142172984+ffelixg@users.noreply.github.com>
2024-07-06 13:29:22 +0200

    proper refcounting, write nulls

--- ziggin/src/root.zig
+++ ziggin/src/root.zig
@@ -30,12 +30,6 @@ const ArrowSchema = packed struct {
     private_data: ?*anyopaque,
 };
 
-// const ArrowBuffers = packed struct {
-//     null_buffer: ?[*]bool,
-//     main_buffer: ?[*]anyopaque,
-
-// }
-
 const ArrowArray = packed struct {
     // Array data description
     length: i64,
@@ -65,6 +59,7 @@ const allocator = gpa.allocator();
 
 const BcpInfo = struct {
     writer: *fn (self: *Column, file: *std.fs.File) void,
+    // writer: @TypeOf(write_l),
     bytes_prefix: u16,
     bytes_data: u16,
     dtype_name: [*:0]u8,
@@ -72,6 +67,7 @@ const BcpInfo = struct {
     fn init(
         // self: *BcpInfo,
         writer: fn (self: *Column, file: *std.fs.File) void,
+        // writer: @TypeOf(write_l),
         bytes_prefix: anytype,
         bytes_data: u16,
         comptime dtype_name: []const u8,
@@ -115,29 +111,17 @@ const Column = struct {
     bcp_info: BcpInfo,
 
     fn deinit(self: Column) void {
-        // print("so clean", .{});
-        py.Py_DecRef(self._schema_capsule);
-        py.Py_DecRef(self._chunk_generator);
+        py.Py_DECREF(self._schema_capsule);
+        py.Py_DECREF(self._chunk_generator);
         py.Py_XDECREF(self._current_array_capsule);
-
-        // _ = self;
-        // comptime std.debug.panic("fix");
-
-        // handled by capsule garbage collection
-        // if (self.schema.release) |release| {
-        //     release(@constCast(&self.schema));
-        // }
-        // if (self.current_array.release) |release| {
-        //     release(@constCast(&self.current_array));
-        // }
-        // self.bcp_info.deinit();
+        // self.bcp_info.deinit(); // TODO ??
     }
 
     fn get_next_array(self: *Column) !bool {
         // return false if no more data
         // py.Py_XDECREF(self._current_array_capsule)
         if (self._current_array_capsule) |capsule| {
-            defer py.Py_DecRef(capsule);
+            defer py.Py_DECREF(capsule);
             self._current_array_capsule = null;
         }
         const array_capsule = py.PyIter_Next(self._chunk_generator) orelse {
@@ -146,7 +130,7 @@ const Column = struct {
             }
             return false;
         };
-        defer py.Py_DecRef(array_capsule);
+        defer py.Py_DECREF(array_capsule);
         const array_ptr = py.PyCapsule_GetPointer(array_capsule, "arrow_array") orelse return Err.PyError;
         const current_array_ptr: *ArrowArray = @alignCast(@ptrCast(array_ptr));
         self.next_index = 0;
@@ -163,22 +147,26 @@ const Column = struct {
 const formats = enum { l, d };
 
 inline fn write(self: *Column, file: *std.fs.File, format: formats) void {
-    _ = self;
-    // _ = file;
     if (format == .d) {
         print("case1\n", .{});
     } else {
         print("case2\n", .{});
     }
-    const tp = switch (format) {
-        inline formats.l => i64,
+    const types = switch (format) {
+        inline formats.l => .{ i64, i8 },
         else => comptime unreachable,
     };
-    const val: tp = 5;
-    // _ = val;
-    // file.write(@as([@sizeOf(tp)]u8, val));
+    const type_data = types[0];
+    const val: type_data = 5;
     const asbytes: [*]u8 = @constCast(@ptrCast(&val));
-    _ = file.write(asbytes[0..@sizeOf(tp)]) catch unreachable;
+    _ = file.write(asbytes[0..@sizeOf(type_data)]) catch unreachable;
+
+    if (self.nullable()) {
+        const type_prefix = types[1];
+        const val2: type_prefix = -1;
+        const asbytes2: [*]u8 = @constCast(@ptrCast(&val2));
+        _ = file.write(asbytes2[0..@sizeOf(type_prefix)]) catch unreachable;
+    }
 }
 
 fn write_l(self: *Column, file: *std.fs.File) void {
@@ -198,9 +186,9 @@ fn write_arrow(module: ?*PyObject, args: ?*PyObject) callconv(.C) ?*PyObject {
 
 fn _write_arrow(args: ?*PyObject) !?*PyObject {
     const list_schema_capsules = py.PySequence_GetItem(args, 0) orelse return Err.PyError;
-    defer py.Py_DecRef(list_schema_capsules);
+    defer py.Py_DECREF(list_schema_capsules);
     const list_array_capsule_generators = py.PySequence_GetItem(args, 1) orelse return Err.PyError;
-    defer py.Py_DecRef(list_array_capsule_generators);
+    defer py.Py_DECREF(list_array_capsule_generators);
     const nr_columns = py.PyObject_Length(list_schema_capsules);
     if (nr_columns == -1) {
         return null;
@@ -218,55 +206,20 @@ fn _write_arrow(args: ?*PyObject) !?*PyObject {
     for (columns_slice, 0..) |*col, i_col| {
         print("i_col {}\n", .{i_col});
 
-        var chunk_generator: *PyObject = undefined;
-        var schema: *ArrowSchema = undefined;
-        var capsule_schema: *PyObject = undefined;
-        {
-            capsule_schema = py.PySequence_GetItem(list_schema_capsules, @intCast(i_col)) orelse return Err.PyError;
-            errdefer py.Py_DecRef(capsule_schema);
-            chunk_generator = py.PySequence_GetItem(list_array_capsule_generators, @intCast(i_col)) orelse return Err.PyError;
-            errdefer py.Py_DecRef(chunk_generator);
-            const schema_ptr = py.PyCapsule_GetPointer(capsule_schema, "arrow_schema") orelse return Err.PyError;
-            schema = @alignCast(@ptrCast(schema_ptr));
-        }
-
-        // const x: comptime_int = 5;
-
-        // const dumdum = switch (i_col) {
-        //     0 => struct {
-        //         fn writer(self_inner: Column, file: std.fs.File) void {
-        //             print("111in write: col {}\n", .{x});
-        //             print("111return from write {}\n", .{file.write(self_inner.schema.format[0..2]) catch unreachable});
-        //         }
-        //     },
-        //     1 => struct {
-        //         fn writer(self_inner: Column, file: std.fs.File) void {
-        //             print("222in write: col {}\n", .{x});
-        //             print("222return from write {}\n", .{file.write(self_inner.schema.format[0..2]) catch unreachable});
-        //         }
-        //     },
-        //     else => unreachable,
-        // };
-        // const dummy = struct {
-        //     fn writer(self_inner: Column, file: std.fs.File) void {
-        //         print("in write: col {}\n", .{x});
-        //         print("return from write {}\n", .{file.write(self_inner.schema.format[0..2]) catch unreachable});
-        //     }
-        // };
+        const capsule_schema = py.PySequence_GetItem(list_schema_capsules, @intCast(i_col)) orelse return Err.PyError;
+        defer py.Py_DECREF(capsule_schema);
+        const chunk_generator = py.PySequence_GetItem(list_array_capsule_generators, @intCast(i_col)) orelse return Err.PyError;
+        defer py.Py_DECREF(chunk_generator);
+        const schema_ptr = py.PyCapsule_GetPointer(capsule_schema, "arrow_schema") orelse return Err.PyError;
+        const schema: *ArrowSchema = @alignCast(@ptrCast(schema_ptr));
 
         col.* = Column{
-            ._chunk_generator = chunk_generator,
+            ._chunk_generator = py.Py_NewRef(chunk_generator),
             .current_array = undefined,
             .schema = schema.*,
-            ._schema_capsule = capsule_schema,
+            ._schema_capsule = py.Py_NewRef(capsule_schema),
             ._current_array_capsule = null,
             .next_index = 0,
-            // .writer = @constCast(&dumdum.writer),
-            // .writer = @constCast(switch (i_col) {
-            //     0 => &write1,
-            //     1 => &write2,
-            //     else => unreachable,
-            // }),
             .bcp_info = undefined,
         };
         n_allocated_columns += 1;
@@ -276,8 +229,6 @@ fn _write_arrow(args: ?*PyObject) !?*PyObject {
             return Err.PyError;
         }
 
-        // const len = std.mem.len(col.schema.format);
-        // var bcp_info: BcpInfo = undefined;
         const fmt = col.schema.format[0..std.mem.len(col.schema.format)];
         col.bcp_info = try BcpInfo.from_format(fmt, col.nullable());
 
